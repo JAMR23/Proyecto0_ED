1deaaff7160138cef10c56377f202c39ee01206d
JAMR23 <jamr237@gmail.com>
2026-05-02 16:07:46 -0600

    Add documentation comments to Usuario.h
    Added file documentation for Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,1 +1,8 @@
+/**
+ * @file Usuario.h
+ * @author José Marín
+ * @date 2026-05-02
+ * @brief Defíne los atributos y metodos que va a tener cada tipo de usuario creado en la ejecución.
+*/
 
+#pragma once
