e4dcdb6c609294c679034b031fb3afa83e6cc559
JAMR23 <jamr237@gmail.com>
2026-05-03 22:05:55 -0600

    Delete Usuario.h

--- Usuario.h
+++
@@ -1,8 +0,0 @@
-/**
- * @file Usuario.h
- * @author José Marín
- * @date 2026-05-02
- * @brief Defíne los atributos y metodos que va a tener cada tipo de usuario creado en la ejecución.
-*/
-
-#pragma once
